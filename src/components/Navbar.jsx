f8af0919feefd5ee8198321014ae878a0e9c50ec
Barun Kumar <77458180+thebarunkumar@users.noreply.github.com>
2023-12-22 17:27:27 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return (
+        <>
+            <div className="navbar">
+                <h1>Language Translator</h1>
+            </div>
+        </>
+    );
+};
